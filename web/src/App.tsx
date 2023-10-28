69ba0163c65818aa5d10d8e81cbabcd0a62d2675
Priscila Zeferino <priscila.zeferino23@gmail.com>
2023-10-28 10:21:09 -0300

    added footer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,15 @@
-import { NavBar } from './components/navbar';
+import { Footer } from './components/footer'
+import { NavBar } from './components/navbar'
 
 function App () {
-
   return (
-    <main>
-      <NavBar></NavBar>
-    </main>
+    <>
+      <div className='flex flex-col min-h-screen'>
+        <NavBar/>
+        <main className='flex-grow'></main>
+        <Footer/>
+      </div>
+    </>
   )
 }
 
---
+++ web/src/components/footer.tsx
@@ -0,0 +1,11 @@
+export function Footer () {
+  return (
+    <>
+      <footer className='p-4 bg-gray-800 text-gray-50'>
+        <div className='container mx-auto text-center'>
+          <p>Desenvolvido com ❤️ por Priscila Zeferino</p>
+        </div>
+      </footer>
+    </>
+  )
+}
