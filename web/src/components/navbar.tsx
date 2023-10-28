74b123619d13e84e1c8e06be2e2c4d3cd14b0c18
Priscila Zeferino <priscila.zeferino23@gmail.com>
2023-10-28 10:11:19 -0300

    menu responsivo

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -2,11 +2,11 @@ import { Button } from './ui/button'
 
 export function NavBar () {
   return (
-    <nav className='border-b flex items-center justify-between p-2 lg:px-8'>
-      <div className='logo'>Study.ia</div>
-      <div className='flex justify-around my-2'>
-        <Button className='flex px-10 mx-2'>github</Button>
-        <Button className='flex px-10 mx-2'>estudar</Button>
+    <nav className='border-b flex flex-col lg:flex-row lg:items-center lg:justify-between p-2 lg:px-8'>
+      <div className='logo text-center lg:text-left'>Study.ia</div>
+      <div className='flex flex-col lg:flex-row lg:items-center lg:justify-around lg:my-2'>
+        <Button className='flex px-6 lg:px-10 my-2 lg:my-0 lg:mx-2'>github</Button>
+        <Button className='flex px-6 lg:px-10 my-2 lg:my-0 lg:mx-2'>estudar</Button>
       </div>
     </nav>
   )
